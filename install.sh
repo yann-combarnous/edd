315091b51dd926d52dad3d3ff9cbfaa8065a8760
yann <yann.combarnous@gmail.com>
2018-12-26 20:06:02 +0100

    Change theme, add desktop shotcuts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible git python-psutil
-ansible-pull --vault-id @prompt -U https://github.com/xunilog/edd.git 
+ansible-pull --vault-id @prompt -U https://github.com/xunilog/edd.git linux.yml
