5c07a6f71cdddfcc3ff479dd99af49566dde7efd
yann <yann.combarnous@gmail.com>
2018-12-26 20:52:48 +0100

    Correct command line

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible git python-psutil
-ansible-pull --ask-vault-pass -U https://github.com/xunilog/edd.git linux.yml
+ansible-pull -U https://github.com/xunilog/edd.git --ask-vault-pass linuxmint.yml
