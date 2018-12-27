6b247f462a11f16ecce8edb45bc12a47c8f2731b
yann <yann.combarnous@gmail.com>
2018-12-27 14:00:49 +0100

    Add power settings to playbook

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible git python-psutil
-ansible-pull -U https://github.com/xunilog/edd.git -K -f linuxmint.yml
+ansible-pull -U https://github.com/xunilog/edd.git -K -f linux.yml
