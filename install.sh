81925dcde833bb64408b37b8dacc64ce74b09154
yann <yann.combarnous@gmail.com>
2018-12-27 09:59:48 +0100

    Separate root tasks from user ones

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-add-apt-repository -y ppa:ansible/ansible
-apt-get update
-apt-get install -y ansible git python-psutil
-ansible-pull -U https://github.com/xunilog/edd.git linuxmint.yml
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible git python-psutil
+ansible-pull -U https://github.com/xunilog/edd.git -K -f linuxmint.yml
