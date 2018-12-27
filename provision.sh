03bfbbf125be43811116a98033888ef1c77d4ae9
yann <yann.combarnous@gmail.com>
2018-12-27 23:30:30 +0100

    Start printer role

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible git python-psutil
-ansible-pull -U https://github.com/xunilog/edd.git -K -f linux.yml
+ansible-pull -U https://github.com/xunilog/edd.git -K provision.yml --clean --purge --full -C master
