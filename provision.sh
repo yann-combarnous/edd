1798acd8583bf94b5f65e24650f37db0f004cf60
yann <yann.combarnous@gmail.com>
2018-12-31 17:22:35 +0100

    Add system upgrade on provision script

--- provision.sh
+++ provision.sh
@@ -2,4 +2,5 @@
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible git python-psutil
+sudo mintupdate-cli upgrade -y
 ansible-pull -U https://github.com/xunilog/edd.git -K provision.yml --clean --purge --full -C master
